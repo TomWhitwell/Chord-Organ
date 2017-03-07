992525ae07b4723e673cb50c2eb2535990fc7876
Martin Wood-Mitrovski <mdk@relivethefuture.com>
2017-03-07 15:22:42 +0100

    Update waveform 8, not 7 twice. :)

--- Chord-Organ/Chord-Organ.ino
+++ Chord-Organ/Chord-Organ.ino
@@ -352,7 +352,7 @@ void updateSines(){
     waveform5.frequency(FREQ[4]);
     waveform6.frequency(FREQ[5]);
     waveform7.frequency(FREQ[6]);
-    waveform7.frequency(FREQ[7]);
+    waveform8.frequency(FREQ[7]);
 
 
     AudioInterrupts();
